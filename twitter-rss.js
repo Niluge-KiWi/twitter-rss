f787a16da2e0ad2bf3694418ad3d86446aa28364
Thomas Riccardi <riccardi.thomas@gmail.com>
2022-03-01 21:25:00 +0100

    use alternate instance for nitter: nitter.fdn.fr -> nitter.42l.fr
    nitter.fdn.fr has a broken users homepage (no tweet)

--- twitter-rss.js
+++ twitter-rss.js
@@ -111,7 +111,7 @@ async.map(config.follow, function (screen_name, cb) {
       title: user.infos.name + ' (@' + user.infos.screen_name + ') Twitter Timeline',
       description: user.infos.description,
       feed_url: getUrl(user.infos.screen_name),
-      site_url: 'https://nitter.fdn.fr/' + user.infos.screen_name,
+      site_url: 'https://nitter.42l.fr/' + user.infos.screen_name,
       image_url: user.infos.profile_image_url,
       author: user.infos.name + ' (@' + user.infos.screen_name + ')',
       language: user.infos.lang
